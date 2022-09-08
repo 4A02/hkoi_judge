b820998e292e7484d4e465c855f6a8b2b0b2ef9a
Cho Lai Lam <107162584+4A02@users.noreply.github.com>
2022-09-08 11:05:54 +0800

    Create d101.py

---
+++ d101.py
@@ -0,0 +1,5 @@
+phone=input()
+if (phone[0]=='2') or (phone[0]=='3'):    #find the first chracter of the inputted string to compare
+    print('Fixed')
+elif (phone[0]=='5') or (phone[0]=='6') or (phone[0]=='9'):
+    print('Mobile')
